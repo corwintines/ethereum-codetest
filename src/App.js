61d0c7e4a13988e6ad9ffa8af848359bbb4a4c05
Corwin Smith <corwinsmith@Corwins-MBP.hitronhub.home>
2021-04-25 16:19:13 -0600

    feat: router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+// Libraries
+import React from 'react'
+import { 
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom'
+
+// Styles
 import './App.css';
 
 const App = () => {
+
+
   return (
-    <p> hello world </p>
+    <Router>
+      <div>
+        <nav>
+          <h2>Democon</h2>
+          <ul>
+            <li>
+              <Link to='/'>Talks</Link>
+            </li>
+            <li>
+              <Link to='/speakers/'>Speakers</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path='/' exact>Talks</Route>
+        <Route path='/details/:id'>Details</Route>
+        <Route path='/speakers/' exact>Speakers</Route>
+        <Route path='/speakers/:id'>Speaker</Route>
+      </div>
+    </Router>
   );
 }
 
