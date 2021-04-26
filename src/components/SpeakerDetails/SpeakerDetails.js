6270ae9a5c95751b6f141ba17d4b6e63252244a8
Corwin Smith <corwinsmith@Corwins-MBP.hitronhub.home>
2021-04-25 19:03:08 -0600

    feat: added titles

--- src/components/SpeakerDetails/SpeakerDetails.js
+++ src/components/SpeakerDetails/SpeakerDetails.js
@@ -6,13 +6,20 @@ import './SpeakerDetails.css'
 
 const SpeakerDetails = (props) => {
   const {
-    speaker
+    speaker,
+    talks
   } = props
+
+  const filteredTalks = talks.filter((talk) => {
+    return speaker.submissions.includes(talk.code)
+  })
+  const filteredTalkTitles = filteredTalks.map((talk) => talk.title)
 
   return (
     <div className="SpeakerDetailsContainer">
       <h3>{speaker.name}</h3>
       <p>{`Biography: ${speaker.biography}`}</p>
+      <p>{`Talks: ${filteredTalkTitles.join(', ')}`}</p>
       <hr />
     </div>
     
