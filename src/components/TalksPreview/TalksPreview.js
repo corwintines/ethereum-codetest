7aafddac9c491c58b7ed75a4db1cb14fc9ae3ebb
Corwin Smith <corwinsmith@Corwins-MBP.hitronhub.home>
2021-04-25 18:24:29 -0600

    feat: talks preview

--- src/components/TalksPreview/TalksPreview.js
+++ src/components/TalksPreview/TalksPreview.js
@@ -1,16 +1,29 @@
 // Libraries
 import React from 'react'
+import { 
+  Link,
+} from 'react-router-dom'
 
 // Styles
 import './TalksPreview.css'
 
 const TalksPreview = (props) => {
   const {
-    talk
+    talk,
   } = props
 
+  const startTime = new Date(talk.slot.start)
+  const endTime = new Date(talk.slot.end)
+  const speakers = talk.speakers.map((speaker) => speaker.name).join(', ')
   return (
-    <p>{talk.title}</p>
+    <div className="TalksPreviewContainer">
+      <h3>{talk.title}</h3>
+      <h5>{`${startTime.toTimeString()} - ${endTime.toTimeString()}`}</h5>
+      <p>{talk.abstract}</p>
+      <p>{`Speakers: ${speakers}`}</p>
+      <Link to={`/details/${talk.code}`}>More</Link>
+      <hr />
+    </div>
   )
 }
 
